83e4fffc50ab667530d33740c820a3f2abba468e
Egemen <egemen.kaba@students.fhnw.ch>
2019-04-30 21:54:21 +0200

    fixed serialization and added the possibility to serialize complex objects if needed

--- src/main/java/ch/fhnw/digibp/ClothingLookup.java
+++ src/main/java/ch/fhnw/digibp/ClothingLookup.java
@@ -2,6 +2,8 @@ package ch.fhnw.digibp;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,18 +12,18 @@ import java.util.logging.Logger;
 public class ClothingLookup implements JavaDelegate {
     private final static Logger LOG = Logger.getLogger("CLOTHING-LOOKUP");
 
-    private final static String peterHahnStrickjackeCoral = "https://mosaic03.ztat" +
+    private final static String peterHahnStrickjackeCoralURL = "https://mosaic03.ztat" +
             ".net/vgs/media/packshot/pdp-zoom/PE/R2/1E/00/KG/11/PER21E00K-G11@10.1.jpg";
+    private final static String peterHahnStrickjackeCoralDSC = "Peter Hahn Strickjacke Coral";
 
     @Override
     public void execute(DelegateExecution delegateExecution) {
         LOG.info("I have looked up some clothes");
+        List<String> singlePossibilities = new LinkedList<>();
+        singlePossibilities.add(peterHahnStrickjackeCoralURL);
 
-        List<PictureDescription> singlePossibilities = new LinkedList<PictureDescription>();
-        singlePossibilities.add(new PictureDescription(peterHahnStrickjackeCoral, "Peter Hahn Strickjacke Coral"));
-
-        List<PictureDescription> setPossibilities = new LinkedList<PictureDescription>();
-        setPossibilities.add(new PictureDescription(peterHahnStrickjackeCoral, "Peter Hahn Strickjacke Coral"));
+        List<String> setPossibilities = new LinkedList<>();
+        setPossibilities.add(peterHahnStrickjackeCoralURL);
 
         if (delegateExecution.getVariable("clothing") == null) {
             // whole set
@@ -37,7 +39,7 @@ public class ClothingLookup implements JavaDelegate {
         private String url;
         private String description;
 
-        public PictureDescription(String url, String description) {
+        PictureDescription(String url, String description) {
             this.url = url;
             this.description = description;
         }
