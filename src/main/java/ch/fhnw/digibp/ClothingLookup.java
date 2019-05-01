692c536535e8d471c8b76fd8e354e49358ac3d2f
Egemen <egemen.kaba@students.fhnw.ch>
2019-05-01 14:08:55 +0200

    added custom serialization for the object containing the picture url, its description and a price tag

--- src/main/java/ch/fhnw/digibp/ClothingLookup.java
+++ src/main/java/ch/fhnw/digibp/ClothingLookup.java
@@ -12,36 +12,56 @@ import java.util.logging.Logger;
 public class ClothingLookup implements JavaDelegate {
     private final static Logger LOG = Logger.getLogger("CLOTHING-LOOKUP");
 
-    private final static String peterHahnStrickjackeCoralURL = "https://mosaic03.ztat" +
-            ".net/vgs/media/packshot/pdp-zoom/PE/R2/1E/00/KG/11/PER21E00K-G11@10.1.jpg";
-    private final static String peterHahnStrickjackeCoralDSC = "Peter Hahn Strickjacke Coral";
+    private PictureDescription peterHahnStrickJackeCoral = new PictureDescription(
+                "https://mosaic03.ztat.net/vgs/media/packshot/pdp-zoom/PE/R2/1E/00/KG/11/PER21E00K-G11@10.1.jpg",
+                "Peter Hahn Strickjacke Coral",
+                17.50);
 
     @Override
     public void execute(DelegateExecution delegateExecution) {
-        LOG.info("I have looked up some clothes");
-        List<String> singlePossibilities = new LinkedList<>();
-        singlePossibilities.add(peterHahnStrickjackeCoralURL);
+        LOG.info("Looking up some clothes");
 
-        List<String> setPossibilities = new LinkedList<>();
-        setPossibilities.add(peterHahnStrickjackeCoralURL);
+        List<PictureDescription> singlePossibilities = new LinkedList<>();
+        singlePossibilities.add(peterHahnStrickJackeCoral);
+
+        List<PictureDescription> setPossibilities = new LinkedList<>();
+        setPossibilities.add(peterHahnStrickJackeCoral);
+
+        ObjectValue singleItems =
+                Variables.objectValue(singlePossibilities).serializationDataFormat("application/json").create();
+
+        ObjectValue setItems =
+                Variables.objectValue(setPossibilities).serializationDataFormat("application/json").create();
 
         if (delegateExecution.getVariable("clothing") == null) {
             // whole set
-            delegateExecution.setVariable("possibilities", setPossibilities);
+            delegateExecution.setVariable("possibilities", setItems);
         } else {
             // only one item
-            delegateExecution.setVariable("possibilities", singlePossibilities);
+            delegateExecution.setVariable("possibilities", singleItems);
         }
+
+        // dedicated flag indicating that the variables are ready for pickup
         delegateExecution.setVariable("readyForPickup", "true");
     }
 
     private class PictureDescription {
         private String url;
         private String description;
+        private Double price;
 
-        PictureDescription(String url, String description) {
+        public PictureDescription(String url, String description, Double price) {
             this.url = url;
             this.description = description;
+            this.price = price;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
         }
 
         public String getUrl() {
@@ -58,6 +78,15 @@ public class ClothingLookup implements JavaDelegate {
 
         public void setDescription(String description) {
             this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return "PictureDescription{" +
+                    "url='" + url + '\'' +
+                    ", description='" + description + '\'' +
+                    ", price=" + price +
+                    '}';
         }
     }
 }
