61d48bee7686302cef0d4f00163493aa7d8772aa
Egemen <egemen.kaba@students.fhnw.ch>
2019-05-04 15:38:32 +0200

    partner lookup for mail and classification, digitalization of xor

--- src/main/java/ch/fhnw/digibp/PaymentToPartner.java
+++ src/main/java/ch/fhnw/digibp/PaymentToPartner.java
@@ -3,9 +3,13 @@ package ch.fhnw.digibp;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import java.util.logging.Logger;
+
 public class PaymentToPartner implements JavaDelegate {
+    private final static Logger LOG = Logger.getLogger("PAYMENT-TO-PARTNER");
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-
+        LOG.info("Paying our partner its share");
     }
 }
