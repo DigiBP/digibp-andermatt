80a0778d7e8866ef60aec354efb74f1cffbda4c3
Egemen <egemen.kaba@students.fhnw.ch>
2019-05-18 17:05:06 +0200

    debugging variables

--- src/main/java/ch/fhnw/digibp/InitializeDecisionVariables.java
+++ src/main/java/ch/fhnw/digibp/InitializeDecisionVariables.java
@@ -4,10 +4,15 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class InitializeDecisionVariables implements JavaDelegate {
+    private final static Logger LOG = Logger.getLogger("INIT");
+
     @Override
     public void execute(DelegateExecution delegateExecution) {
+        LOG.info(String.format("Variables before init: %s", delegateExecution.getVariables()));
+
         String sexAndSize   = (String) Optional.ofNullable(delegateExecution.getVariable("sexAndSize")).orElse("");
         String delivery     = (String) Optional.ofNullable(delegateExecution.getVariable("delivery")).orElse("");
         String priceClass   = (String) Optional.ofNullable(delegateExecution.getVariable("priceClass")).orElse(  "");
